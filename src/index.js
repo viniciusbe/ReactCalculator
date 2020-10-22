97ef737d3ffa896f63510dce7117c426ee8c8a2b
viniciusbe <vinicius.bernardesds@gmail.com>
2020-10-21 21:18:42 -0300

    Att title

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <div>
-    <h1>Calculadora</h1>
+    <h1>Calculator</h1>
     <Calculator></Calculator>
   </div>
-  ,document.getElementById('root')
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
